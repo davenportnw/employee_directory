9a06243f1b2f089485dd2e47db66f5a8f07b34b6
davenportnw <davenportnw@gmail.com>
2020-08-31 17:38:54 -0500

    Adjust search feature.

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import SearchBar from "./components/SerachBar";
+import employees from "./employees.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.employees to the employee json array.
+  state = {
+    employees,
+    searchEmployee = ""
+  };
+  //Handling Search Bar input
+  handleInput = (employees) => {
+    this.setState({ searchEmployee: employees.target.value});
+  }
+  //Filter Search
+  render() {
+    let filteredSearch = this.state.employees.filter((employee) =>{
+      return employee.name.toLowerCase().includes(this.state.searchEmployee)
+    })
+  
+    return (
+      <div>
+        <SearchBar handleInput={this.handleInput}/>
+        <Wrapper>
+          {this.state.employees.map(employee => (
+            <EmployeeCard
+              id={employee.id}
+              key={employee.id}
+              name={employee.name}
+              image={employee.image}
+              occupation={employee.occupation}
+              email={employee.email}
+            />
+          ))}
+        </Wrapper>
+      </div>
+    );
+  }
 }
 
 export default App;
--- employee_directory/src/components/SerachBar/index.js
+++ employee_directory/src/components/SerachBar/index.js
@@ -1,1 +1,20 @@
-import React from "react";
+import React, {Component} from "react";
+import Wrapper from "../Wrapper";
+import EmployeeCard from "../EmployeeCard";
+import { MDBCol, MDBIcon } from "mdbreact";
+
+import employees from "employee_directory/src/employees.json";
+
+const SearchBar = (props) => {
+    return (
+      <MDBCol md="6">
+        <form className="form-inline mt-4 mb-4">
+          <MDBIcon icon="search" />
+          <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" onChange={props.handleInput}/>
+        </form>
+      </MDBCol>
+    );
+  }
+  
+
+export default SearchBar;
